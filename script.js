6b26ed7cd8a53df3fabc5f563a14d3287577f0ce
Kipngetich222 <123062517+Kipngetich222@users.noreply.github.com>
2023-06-21 17:26:39 +0300

    Frequently asked question user interface

---
+++ script.js
@@ -0,0 +1,21 @@
+let accordion = document.querySelectorAll(".content-container");
+let container = document.querySelector(".answer")
+
+//accordion.addEventListener("click", open);
+
+
+function openn() {
+    container.style.display = "flex";
+}
+
+
+
+
+// for (let i = 0; i < vic.length; i++) {
+//     const element = vic[i];
+
+//     element.addEventListener("click", function () {
+//         this.classList.toggle('active');
+//     })
+    
+// }
